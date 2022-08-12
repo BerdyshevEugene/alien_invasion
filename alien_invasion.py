99f80021a8de68f1b0c2d6ff1c2a3313cbe1df1c
BerdyshevEugene <berdyshevevg@mail.ru>
2022-08-12 19:21:26 +0700

    1-st ver.

---
+++ alien_invasion.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Инициализирует pygame, settings и объект экрана.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (40, 45, 101)
+    # Создание корабля.
+    ship = Ship(screen)
+
+    # Запуск основного цикла игры.
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+run_game()
