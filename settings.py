44d1c6e528dd69f2efa7ddd001db989d6c6ed4e8
BerdyshevEugene <berdyshevevg@mail.ru>
2022-08-17 23:55:53 +0800

    2-nd ver. Aliens and backround added

--- settings.py
+++ settings.py
@@ -11,14 +11,14 @@ class Settings():
         self.bg_color = (7, 11, 39)  # фон нерабочий
         bg = pygame.image.load('images/space.bmp')
         self.bg = pygame.transform.scale(bg, (1400, 700))
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 5
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 2
         # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
         self.fleet_direction = 1
         # Параметры пули
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 10
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = 96, 174, 200
         self.bullets_allowed = 3
