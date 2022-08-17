c28bf1fe22abb76747c3d78aba8ace82e2b21c97
BerdyshevEugene <berdyshevevg@mail.ru>
2022-08-17 22:14:47 +0800

    2-nd ver. Aliens and backround added

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -13,13 +14,19 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (40, 45, 101)
     # Создание корабля.
     ship = Ship(ai_settings, screen)
+     # Создание группы для хранения пуль.
+    bullets = Group()
+    # Создание пришельца.
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Запуск основного цикла игры.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 run_game()
--- settings.py
+++ settings.py
@@ -1,15 +1,24 @@
+import pygame
+
 class Settings():
     """Класс для хранения всех настроек игры Alien Invasion."""
 
     def __init__(self):
         """Инициализирует настройки игры."""
         # Параметры экрана
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 1400
+        self.screen_height = 700
+        self.bg_color = (7, 11, 39)  # фон нерабочий
+        bg = pygame.image.load('images/space.bmp')
+        self.bg = pygame.transform.scale(bg, (1400, 700))
         self.ship_speed_factor = 1.5
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
         # Параметры пули
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
